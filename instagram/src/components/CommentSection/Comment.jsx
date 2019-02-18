f896cb3a08eaddb9a76ed23c301d7ab75ee062f8
Joel Bartlett <joel@murbar.com>
2019-02-18 17:14:46 -0500

    Check prop types

--- instagram/src/components/CommentSection/Comment.jsx
+++ instagram/src/components/CommentSection/Comment.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Comment = ({ data }) => {
   return <div className="comments-section-comment">{data.text}</div>;
 };
 
 export default Comment;
+
+Comment.propTypes = {
+  data: PropTypes.shape({
+    username: PropTypes.string,
+    text: PropTypes.string
+  })
+};
