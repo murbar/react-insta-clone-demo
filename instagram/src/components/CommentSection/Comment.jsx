7b01aff59255e1c5c1ad8a90c12cc9fa2c5f3176
Joel Bartlett <joel@murbar.com>
2019-02-18 17:06:47 -0500

    Change class name

--- instagram/src/components/CommentSection/Comment.jsx
+++ instagram/src/components/CommentSection/Comment.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Comment = ({ data }) => {
-  return <div className="comments-comment">{data.text}</div>;
+  return <div className="comments-section-comment">{data.text}</div>;
 };
 
 export default Comment;
