686f8f3ad6b04e3e25d10e74667e9dc268fb9df4
Joel Bartlett <joel@murbar.com>
2019-02-18 18:19:44 -0500

    Convert to SFC

--- instagram/src/components/SearchBar/SearchBar.jsx
+++ instagram/src/components/SearchBar/SearchBar.jsx
@@ -1,20 +1,14 @@
 import React from 'react';
 import './SearchBar.css';
 
-class SearchBar extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-  render() {
-    return (
-      <div className="search-bar">
-        logo
-        <input type="search" placeholder="Search" />
-        icons
-      </div>
-    );
-  }
-}
+const SearchBar = props => {
+  return (
+    <div className="search-bar">
+      logo
+      <input type="search" placeholder="Search" />
+      icons
+    </div>
+  );
+};
 
 export default SearchBar;
