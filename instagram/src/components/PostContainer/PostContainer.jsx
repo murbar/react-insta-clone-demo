748a9ade4729286993907a3aaff598eece5437f5
Joel Bartlett <joel@murbar.com>
2019-02-18 18:21:35 -0500

    Convert to SFC

--- instagram/src/components/PostContainer/PostContainer.jsx
+++ instagram/src/components/PostContainer/PostContainer.jsx
@@ -4,25 +4,18 @@ import CommentSection from '../CommentSection/CommentSection';
 import Post from './Post';
 import './PostContainer.css';
 
-class PostContainer extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-  render() {
-    const { post } = this.props;
-    return (
-      <div className="post-container">
-        <Post post={post} />
-        <div className="post-time">
-          <Moment fromNow parse="MMMM Do YYYY, hh:mm:ss a">
-            {post.timestamp}
-          </Moment>
-        </div>
-        <CommentSection comments={post.comments} />
+const PostContainer = ({ post }) => {
+  return (
+    <div className="post-container">
+      <Post post={post} />
+      <div className="post-time">
+        <Moment fromNow parse="MMMM Do YYYY, hh:mm:ss a">
+          {post.timestamp}
+        </Moment>
       </div>
-    );
-  }
-}
+      <CommentSection comments={post.comments} />
+    </div>
+  );
+};
 
 export default PostContainer;
