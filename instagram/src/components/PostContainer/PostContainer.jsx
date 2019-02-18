49d9b5e3532fd647ef947f825db5329f123863fe
Joel Bartlett <joel@murbar.com>
2019-02-18 17:52:01 -0500

    Format timestamp

--- instagram/src/components/PostContainer/PostContainer.jsx
+++ instagram/src/components/PostContainer/PostContainer.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Moment from 'react-moment';
 import CommentSection from '../CommentSection/CommentSection';
 import Post from './Post';
 
@@ -12,7 +13,11 @@ class PostContainer extends React.Component {
     return (
       <div className="post-container">
         <Post post={post} />
-        <div className="post-time">{post.timestamp}</div>
+        <div className="post-time">
+          <Moment fromNow parse="MMMM Do YYYY, hh:mm:ss a">
+            {post.timestamp}
+          </Moment>
+        </div>
         <CommentSection comments={post.comments} />
       </div>
     );
