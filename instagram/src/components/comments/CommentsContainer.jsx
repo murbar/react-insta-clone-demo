6a09b5836cb502c24993b0c05d22c430f99488f0
Joel Bartlett <joel@murbar.com>
2019-02-19 17:55:49 -0500

    Convert to function component

--- instagram/src/components/comments/CommentsContainer.jsx
+++ instagram/src/components/comments/CommentsContainer.jsx
@@ -2,25 +2,17 @@ import React from 'react';
 import Comment from './Comment';
 import './comments.css';
 
-class CommentsContainer extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-  render() {
-    const { comments } = this.props;
+const CommentsContainer = ({ comments }) => {
+  if (!comments.length) return null;
 
-    if (!comments.length) return null;
-
-    return (
-      <div className="comments">
-        {comments.map(c => (
-          <Comment data={c} key={c.text} />
-        ))}
-      </div>
-    );
-  }
-}
+  return (
+    <div className="comments">
+      {comments.map(c => (
+        <Comment data={c} key={c.text} />
+      ))}
+    </div>
+  );
+};
 
 export default CommentsContainer;
 
