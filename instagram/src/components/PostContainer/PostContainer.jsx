0903a1eac275c56836990a64a9a290463a22fe9c
Joel Bartlett <joel@murbar.com>
2019-02-19 16:17:05 -0500

    Validate prop types

--- instagram/src/components/PostContainer/PostContainer.jsx
+++ instagram/src/components/PostContainer/PostContainer.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Moment from 'react-moment';
+import PropTypes from 'prop-types';
 import CommentSection from '../CommentSection/CommentSection';
 import Post from './Post';
 import './PostContainer.css';
@@ -19,3 +20,14 @@ const PostContainer = ({ post }) => {
 };
 
 export default PostContainer;
+
+PostContainer.propTypes = {
+  post: PropTypes.shape({
+    username: PropTypes.string.isRequired,
+    imageUrl: PropTypes.string,
+    thumbnailUrl: PropTypes.string,
+    likes: PropTypes.number,
+    timestamp: PropTypes.string,
+    comments: PropTypes.array
+  })
+};
