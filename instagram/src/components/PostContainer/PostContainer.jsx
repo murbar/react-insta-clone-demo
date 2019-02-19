d88ae193991c7e9bf124b8846170e72a580739ce
Joel Bartlett <joel@murbar.com>
2019-02-18 19:02:53 -0500

    Move post time

--- instagram/src/components/PostContainer/PostContainer.jsx
+++ instagram/src/components/PostContainer/PostContainer.jsx
@@ -8,12 +8,12 @@ const PostContainer = ({ post }) => {
   return (
     <div className="post-container">
       <Post post={post} />
+      <CommentSection comments={post.comments} />
       <div className="post-time">
         <Moment fromNow parse="MMMM Do YYYY, hh:mm:ss a">
           {post.timestamp}
         </Moment>
       </div>
-      <CommentSection comments={post.comments} />
     </div>
   );
 };
