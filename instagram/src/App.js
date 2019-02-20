177dbe59661575ac19bab1effedf4304e2d34e81
Joel Bartlett <joel@murbar.com>
2019-02-19 22:12:16 -0500

    Add search functionality

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -5,17 +5,54 @@ import PostContainer from './components/posts/PostContainer';
 import './App.css';
 
 class App extends Component {
-  state = { posts: [] };
+  state = { posts: [], filteredPosts: null };
 
   componentDidMount() {
     this.setState({ posts: dummyData });
   }
 
+  filterPosts = searchTerm => {
+    if (!searchTerm) {
+      this.setState({
+        filteredPosts: null,
+        searchTerm: null
+      });
+      return;
+    }
+
+    let filtered = this.state.posts.filter(p => {
+      const normalizedUsername = p.username.toLowerCase();
+      const normalizedSearchTerm = searchTerm.toLowerCase();
+      return normalizedUsername.includes(normalizedSearchTerm);
+    });
+    filtered = filtered.length ? filtered : null;
+    this.setState({
+      filteredPosts: filtered,
+      searchTerm
+    });
+  };
+
+  // currently showing all when search term not found
   render() {
+    const { posts, filteredPosts, searchTerm } = this.state;
+    // const displayPosts = filteredPosts ? filteredPosts : posts;
+    let displayedPosts = posts;
+    if (searchTerm && !filteredPosts) {
+      return (
+        <div className="App">
+          <SearchBar onFilter={this.filterPosts} />
+          <div className="no-results">No results for "{searchTerm}".</div>
+        </div>
+      );
+    }
+    if (filteredPosts) {
+      displayedPosts = filteredPosts;
+    }
+
     return (
       <div className="App">
-        <SearchBar />
-        {this.state.posts.map(p => (
+        <SearchBar onFilter={this.filterPosts} />
+        {displayedPosts.map(p => (
           <PostContainer post={p} key={p.timestamp} />
         ))}
       </div>
--- instagram/src/components/common/SearchBar.jsx
+++ instagram/src/components/common/SearchBar.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import './SearchBar.css';
 
-const SearchBar = props => {
+const SearchBar = ({ onFilter }) => {
+  const onChange = e => {
+    onFilter(e.target.value);
+  };
+
   return (
     <div className="search-bar">
       logo
-      <input type="search" placeholder="Search" />
+      <input type="search" placeholder="Search" onChange={onChange} />
       icons
     </div>
   );
