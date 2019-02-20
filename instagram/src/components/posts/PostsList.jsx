f754d93b6bc3224558b4de682681f1674abe276f
Joel Bartlett <joel@murbar.com>
2019-02-20 18:20:07 -0500

    Add message showing search result count

--- instagram/src/components/posts/PostsList.jsx
+++ instagram/src/components/posts/PostsList.jsx
@@ -4,12 +4,21 @@ import Post from './Post';
 const PostsList = ({ posts, filteredPosts, searchTerm }) => {
   if (searchTerm && !filteredPosts)
     return (
-      <div className="posts-list--no-results">No posts with username matching "{searchTerm}"</div>
+      <div className="posts-list--no-results">
+        No posts with username matching <strong>"{searchTerm}"</strong>
+      </div>
     );
 
-  let displayedPosts = filteredPosts || posts;
+  const displayedPosts = filteredPosts || posts;
+  const showingResults = searchTerm && filteredPosts;
+
   return (
     <div className="posts-list">
+      {showingResults && (
+        <div className="posts-list--results-count">
+          {filteredPosts.length} post(s) matching <strong>"{searchTerm}"</strong>
+        </div>
+      )}
       {displayedPosts.map(p => (
         <Post post={p} key={p.timestamp} />
       ))}
