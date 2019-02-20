d26c247899e5ba21d6d0be42718e9c1a9d387e60
Joel Bartlett <joel@murbar.com>
2019-02-20 16:57:39 -0500

    Build out LoginPage

---
+++ instagram/src/components/login/LoginPage.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+
+const LoginPage = props => {
+  const [username, setUsername] = useState('');
+
+  const handleChange = e => {
+    setUsername(e.target.value);
+  };
+
+  const handleSubmit = e => {
+    // e.preventDefault();
+    console.log(username);
+    localStorage.setItem('username', username);
+  };
+
+  return (
+    <div className="login-page">
+      <h1>Login to continue</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Username" value={username} onChange={handleChange} />
+        <button>Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginPage;
