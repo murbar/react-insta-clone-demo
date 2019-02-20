e3e4875a79b84a6903c2d762594ee127c29c40fe
Joel Bartlett <joel@murbar.com>
2019-02-20 16:57:19 -0500

    Build out authenticate

--- instagram/src/authentication/authenticate.jsx
+++ instagram/src/authentication/authenticate.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
 
-const authenticate = App =>
+const authenticate = App => LoginPage =>
   class extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        loggedIn: false
+      };
+    }
+
+    componentDidMount() {
+      const user = localStorage.getItem('username');
+      if (user) this.setState({ loggedIn: true });
+    }
+
     render() {
-      return <App />;
+      if (this.state.loggedIn) return <App />;
+      return <LoginPage />;
     }
   };
 
