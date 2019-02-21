9b591a8126305759f7bd03b4c7341ba58c461bae
Joel Bartlett <joel@murbar.com>
2019-02-21 16:14:00 -0500

    Style App with styled-components

--- instagram/src/App.jsx
+++ instagram/src/App.jsx
@@ -2,13 +2,19 @@ import React from 'react';
 import PostsPage from './components/posts/PostsPage';
 import authenticate from './authentication/authenticate';
 import LoginPage from './components/login/LoginPage';
+import styled from 'styled-components';
 import './App.css';
+
+const StyledAppDiv = styled.div`
+  margin: 2rem auto;
+  width: 40rem;
+`;
 
 const App = props => {
   return (
-    <div className="App">
+    <StyledAppDiv className="App">
       <PostsPage />
-    </div>
+    </StyledAppDiv>
   );
 };
 
