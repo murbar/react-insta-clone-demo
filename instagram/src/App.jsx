80914cfbf19d50fc47ffc58aafeb47b1b11c0d55
Joel Bartlett <joel@murbar.com>
2019-02-21 16:21:02 -0500

    Update notes to self

--- instagram/src/App.jsx
+++ instagram/src/App.jsx
@@ -23,16 +23,6 @@ export default authenticate(App)(LoginPage);
 /* 
 TODO
 - fuzzy search with https://fusejs.io/
-- add and style iconography
-- style search bar
-- style post controls
 - persist to localStorage
 - delete comment functionality
-- feather icons
-  - user
-  - heart
-  - compass
-  - search
-  - message-circle
-  - more-horizontal
 */
